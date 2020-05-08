5662beaefbaaeeacc751ecad83232cffdc3ce00c
WinterCricket <hodhodhod2003@gmail.com>
2020-05-08 16:54:43 -0500

    start using php loops and arrays

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 
-This would be a lame duck.
+
+ <h1><?php bloginfo("name" ); ?></h1>
+ <p><?php bloginfo("description"); ?></p>
+
+ This would be a lame duck.
 <?php 
 	function greet($name, $color){
 		echo "<h2>Hello, my name is $name and my favorite color is $color.</h2>";
 	}
-	greet("Bob", "yellow");
+	greet("Bob", "runny yellow");
 	greet("Alice", "hard purple");
  ?>
- <h1><?php bloginfo("name" ); ?></h1>
- <p><?php bloginfo("description"); ?></p>
+
+ <?php 
+
+ $names = array("Dogman", "Catman", "Batman", "Catamaranman", "Atmanman");
+ 
+ $count = 1;
+
+ while($count < 54) {
+ 	echo "<li> $count </li>";
+ 	$count++;
+
+ }
+
+  ?>
+ 
