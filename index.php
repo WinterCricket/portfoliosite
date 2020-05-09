44955fedbc411e6aacaa1c02151c3d7f90029ae5
WinterCricket <hodhodhod2003@gmail.com>
2020-05-09 00:54:19 -0500

    post functions for wp

--- index.php
+++ index.php
@@ -1,28 +1,14 @@
 
 
- <h1><?php bloginfo("name" ); ?></h1>
- <p><?php bloginfo("description"); ?></p>
-
- This would be a lame duck.
 <?php 
-	function greet($name, $color){
-		echo "<h2>Hello, my name is $name and my favorite color is $color.</h2>";
-	}
-	greet("Bob", "runny yellow");
-	greet("Alice", "hard purple");
+
+while(have_posts()){
+	the_post();?>
+
+	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	<?php the_content(); ?>
+	<hr>
+
+<?php }
+
  ?>
-
- <?php 
-
- $names = array("Dogman", "Catman", "Batman", "Catamaranman", "Atmanman", "Womanman");
- 
- $i = 0;
-
- while($i < count($names)) {
- 	echo "<li> Here is $names[$i]! </li>";
- 	$i++;
-
- }
-
-  ?>
- 
