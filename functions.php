c3ba2b7bea53ce030c9ab8582deb24d551ee0ed8
WinterCricket <hodhodhod2003@gmail.com>
2020-05-12 23:46:39 -0500

    add body and another enqueue also connect fontawesome to wp

--- functions.php
+++ functions.php
@@ -2,7 +2,12 @@
 
 function folio_files(){ 
 
+	
+
+	wp_enqueue_style("font-awesome", "//use.fontawesome.com/releases/v5.3.1/css/all.css");
+
 	wp_enqueue_style("folio_main_styles", get_stylesheet_uri());
+
 }
 
 add_action("wp_enqueue_script", "folio_files");
--- index.php
+++ index.php
@@ -1,15 +1,63 @@
 
 
-<?php get_header();
+<?php get_header(); ?>
 
-while(have_posts()){
-	the_post();?>
+<h2 class="projects-header">
+		Here are some of my projects
+	</h2>
+	<section id="projects" class="projects">
+		
+		
+		<div>
+			<a href="https://codepen.io/WinterCricket/" target="_blank" class="project project-tile"> <!-- class project -->
+				<img class="thumbnail" src="http://www.publicdomainpictures.net/pictures/20000/velka/prague-astronomical-clock-detail-871291743639AGq.jpg" alt="project">
+				<div class="project-text">Timeless Machine</div>
+			</a>
+		</div>
+		<div>
+			<a href="" target="_blank" class="project project-tile">
+				<img class="thumbnail" src="https://images.unsplash.com/photo-1471623817296-aa07ae5c9f47?dpr=1&auto=compress,format&crop=entropy&fit=crop&w=767&h=511&q=80&cs=tinysrgb" alt="project">
+				<div class="project-text">Fearful Traveler Bed Finder</div>
+			</a>
+		</div>
+		<div>
+			<a href="" target="_blank" class="project project-tile">
+				<img class="thumbnail" src="http://www.bensound.com/bensound-img/tomorrow.jpg" alt="project">
+				<div class="project-text">App Prevents Long Walks Off Short Piers</div>
+			</a>
+		</div>
 
-	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	<?php the_content(); ?>
-	<hr>
+		
+	</section>
 
-<?php }
+	<section id="contact" class="contact">
+		<h2 class="projects-header" >Connect</h2>
+		<div class="header">
+			
+
+			<div>
+				
+			<a id="profile-link" href="https://github.com/WinterCricket" target="_blank" class="contact-specs"><i class="fab fa-github fa-2x"><br>github</i></a>
+			</div>
+			<div>
+				
+				<a href="https://codepen.io/WinterCricket/" target="_blank" class="contact-specs"><i class="fab fa-codepen fa-2x"><br>codepen</i></a>
+			</div>
+
+			<div>
+			<a href="https://www.freecodecamp.org/wintercricket" target="_blank" class="contact-specs"><i class="fab fa-free-code-camp fa-2x"><br> freecodecamp</i></a>
+			</div>
+
+			<div>
+			<a href="mailto:hodhodhod2003@gmail.com" target="_blank" class="contact-specs"><i class="fab fa-google fa-2x"><br>mail</i></a>
+			</div>
+
+		</div>
+
+		
+	</section>
+
+<?php 
 	get_footer();
 
  ?>
